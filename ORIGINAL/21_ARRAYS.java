08f03f56e5176ef2eb8d9879614a128d58ffcf10
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-06-05 12:14:29 +0530

    all

--- ORIGINAL/21_ARRAYS.java
+++ ORIGINAL/21_ARRAYS.java
@@ -13,68 +13,68 @@
             //            \ /                  \ /
             //  Array Declaration       Memory Allocation
 
-class ARRAYS
-{
-    public static void main(String[]args)
-    {
-        // Type of Declaring an array: 
+// class ARRAYS
+// {
+//     public static void main(String[]args)
+//     {
+//         // Type of Declaring an array: 
         
-        // 1) Declaration + Memory Allocation(DIRECTLY)
-        int [] a = new int[5];
+//         // 1) Declaration + Memory Allocation(DIRECTLY)
+//         int [] a = new int[5];
 
-        // 2) Declaration & Memory Allocation (SEPARATELY)
-        float [] b;
-        b = new float[5];
+//         // 2) Declaration & Memory Allocation (SEPARATELY)
+//         float [] b;
+//         b = new float[5];
 
-        // 3) Declaration + Initialization  (In this case JAVA identifies the size automatically, no need for size declaration externally.)
-        double [] c = {1,2,3,6,5,4,7,8,9};
+//         // 3) Declaration + Initialization  (In this case JAVA identifies the size automatically, no need for size declaration externally.)
+//         double [] c = {1,2,3,6,5,4,7,8,9};
 
 
-        // Assigning values in an array:
-        a[0] = 60;             b[0] = 60;
-        a[1] = 70;             b[1] = 70;
-        a[2] = 80;             b[2] = 80;
-        a[3] = 90;             b[3] = 90;
-        a[4] = 100;            b[4] = 100;
+//         // Assigning values in an array:
+//         a[0] = 60;             b[0] = 60;
+//         a[1] = 70;             b[1] = 70;
+//         a[2] = 80;             b[2] = 80;
+//         a[3] = 90;             b[3] = 90;
+//         a[4] = 100;            b[4] = 100;
 
-        // * Arrays are "Mutable"
-        a[1] = 999; // -> Now the value of the elemnt at 1st index is 999
+//         // * Arrays are "Mutable"
+//         a[1] = 999; // -> Now the value of the elemnt at 1st index is 999
 
-        // Size of an Array(length)
-        int len_a = a.length;
-        System.out.println(len_a);
-        int len_b = b.length;
-        System.out.println(len_b);
-        System.out.println(c.length);
+//         // Size of an Array(length)
+//         int len_a = a.length;
+//         System.out.println(len_a);
+//         int len_b = b.length;
+//         System.out.println(len_b);
+//         System.out.println(c.length);
 
-        // Displaying elements of an array:
-        // 1) Manually
-        System.out.println(a[0]);                System.out.println(b[0]);       
-        System.out.println(a[1]);                System.out.println(b[1]);
-        System.out.println(a[2]);                System.out.println(b[2]);
-        System.out.println(a[3]);                System.out.println(b[3]);
-        System.out.println(a[4]);                System.out.println(b[4]);
+//         // Displaying elements of an array:
+//         // 1) Manually
+//         System.out.println(a[0]);                System.out.println(b[0]);       
+//         System.out.println(a[1]);                System.out.println(b[1]);
+//         System.out.println(a[2]);                System.out.println(b[2]);
+//         System.out.println(a[3]);                System.out.println(b[3]);
+//         System.out.println(a[4]);                System.out.println(b[4]);
 
-        // 2) Naive Way:
-        for (int i = 0;i<len_a;i++)
-        {
-            System.out.println(a[i]);
-        }
+//         // 2) Naive Way:
+//         for (int i = 0;i<len_a;i++)
+//         {
+//             System.out.println(a[i]);
+//         }
         
-        // 3) for-each loop in java:
+//         // 3) for-each loop in java:
 
-        // Synatx:
-        // for(data_type "element" : array_name)
-        // {
-        //     System.out.println("element");
-        // }
+//         // Synatx:
+//         // for(data_type "element" : array_name)
+//         // {
+//         //     System.out.println("element");
+//         // }
 
-        for(int element : a)
-        {
-            System.out.println(element);
-        }
-    }
-}
+//         for(int element : a)
+//         {
+//             System.out.println(element);
+//         }
+//     }
+// }
 
 
 /* Multi Dimensional */
@@ -89,57 +89,57 @@ class ARRAYS
 //                                     \ /
 //                 Depends on array Diemnsion([][][] -> 3x3; [][] -> 2x2) 
 
-// class ARRAYS
-// {
-//     public static void main(String[]args)
-//     {
+class ARRAYS
+{
+    public static void main(String[]args)
+    {
 
-//         // Types of declaring an array:
+        // Types of declaring an array:
 
-//         // 1) Declaration + Memory Allocation (DIRECTLY)
-//         int [][] a = new int [2][2];
+        // 1) Declaration + Memory Allocation (DIRECTLY)
+        int [][] a = new int [2][2];
 
-//         // 2) Declaration & Memory Allocation (SEPARATELY)
-//         float [][] b;
-//         b = new float [2][2];
+        // 2) Declaration & Memory Allocation (SEPARATELY)
+        float [][] b;
+        b = new float [2][2];
 
-//         // 3) Declaration + Initialization  (In this case JAVA identifies the size automatically, no need for size declaration externally.)
-//         double [][][] c = {{1,2,3},{4,5,6},{7,8,9}};
+        // 3) Declaration + Initialization  (In this case JAVA identifies the size automatically, no need for size declaration externally.)
+        double [][] c = {{1.0,2.0,3.0},{4.0,5.0,6.0},{7.0,8.0,9.0}};
 
 
-//         // Assigning Values in an array:
-//         a[0][0] = 1;        b[0][0] = 1;
-//         a[0][1] = 2;        b[0][1] = 2;
-//         a[1][0] = 3;        b[1][0] = 3;
-//         a[1][1] = 4;        b[1][1] = 4;
+        // Assigning Values in an array:
+        a[0][0] = 1;        b[0][0] = 1;
+        a[0][1] = 2;        b[0][1] = 2;
+        a[1][0] = 3;        b[1][0] = 3;
+        a[1][1] = 4;        b[1][1] = 4;
 
-//         // * Arrays are "Mutable"
-//         a[1][0] = 999; // -> Now the value of the elemnt at 1st index is 999
+        // * Arrays are "Mutable"
+        a[1][0] = 999; // -> Now the value of the elemnt at 1st index is 999
 
-//         // Size of an Array(length)
-//         int len_a = a.length;
-//         System.out.println(len_a);
-//         int len_b = b.length;
-//         System.out.println(len_b);
+        // Size of an Array(length)
+        int len_a = a.length;
+        System.out.println(len_a);
+        int len_b = b.length;
+        System.out.println(len_b);
 
-//         // Displaying elements of an array:
-//         // 1) Manually
-//         System.out.println(a[0][0]);                System.out.println(b[0][0]);       
-//         System.out.println(a[0][1]);                System.out.println(b[0][0]);
-//         System.out.println(a[1][0]);                System.out.println(a[1][0]);
-//         System.out.println(a[1][1]);                System.out.println(a[1][1]);
+        // Displaying elements of an array:
+        // 1) Manually
+        System.out.println(a[0][0]);                System.out.println(b[0][0]);       
+        System.out.println(a[0][1]);                System.out.println(b[0][0]);
+        System.out.println(a[1][0]);                System.out.println(a[1][0]);
+        System.out.println(a[1][1]);                System.out.println(a[1][1]);
 
-//         // 2) Naive Way:
-//         // (Array - a)
-//         for(int i = 0;i<2;i++)
-//         {
-//             for(int j = 0;j<2;j++)
-//             {
-//                 System.out.print(a[i][j]+" ");
-//             }
-//             System.out.println();
-//         }
+        // 2) Naive Way:
+        // (Array - c)
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
+        }
 
-//     }
-// }
+    }
+}
 
